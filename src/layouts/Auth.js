e04bbb4f3449e7846a321cc6ee8c2cc5405a1c72
Muhammad Hasham Khan <91991265+MuhammadHashamKhan1999@users.noreply.github.com>
2024-04-03 23:07:48 +0500

    Update Auth.js

--- src/layouts/Auth.js
+++ src/layouts/Auth.js
@@ -8,7 +8,7 @@
 * Copyright 2024 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
 
-* Coded by Creative Tim
+* Coded by Creative Tim 
 
 =========================================================
 
